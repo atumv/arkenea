f0e6108189e92c99f87da15ea8754d6bf10da5fe
atumv <atumv.gm@gmail.com>
2022-04-20 17:40:33 +0700

    Remove jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,49 @@
-'use strict';
-$(document).ready(function () {
-  $('.slider__dot-first').on({
-    click: function () {
-      $('.testimonial-first').css('display', 'block').hide().fadeIn(700),
-        $('.testimonial-second').css('display', 'none'),
-        $('.testimonial-third').css('display', 'none'),
-        $('.slider__dot-first').css('background-color', '#f5f5f5'),
-        $('.slider__dot-second').css('background-color', '#aca6a1'),
-        $('.slider__dot-third').css('background-color', '#aca6a1');
-    },
-  }),
-    $('.slider__dot-second').on({
-      click: function () {
-        $('.testimonial-first').css('display', 'none'),
-          $('.testimonial-second').css('display', 'block').hide().fadeIn(700),
-          $('.testimonial-third').css('display', 'none'),
-          $('.slider__dot-first').css('background-color', '#aca6a1'),
-          $('.slider__dot-second').css('background-color', '#f5f5f5'),
-          $('.slider__dot-third').css('background-color', '#aca6a1');
-      },
-    }),
-    $('.slider__dot-third').on({
-      click: function () {
-        $('.testimonial-first').css('display', 'none'),
-          $('.testimonial-second').css('display', 'none'),
-          $('.testimonial-third').css('display', 'block').hide().fadeIn(700),
-          $('.slider__dot-first').css('background-color', '#aca6a1'),
-          $('.slider__dot-second').css('background-color', '#aca6a1'),
-          $('.slider__dot-third').css('background-color', '#f5f5f5');
-      },
-    });
-});
+const firstSliderDot = document.querySelector('.slider__dot-first');
+const secondSliderDot = document.querySelector('.slider__dot-second');
+const thirdSliderDot = document.querySelector('.slider__dot-third');
+const firstTestimonial = document.querySelector('.testimonial-first');
+const secondTestimonial = document.querySelector('.testimonial-second');
+const thirdTestimonial = document.querySelector('.testimonial-third');
+
+firstSliderDot.addEventListener('click', showFirstQuote);
+secondSliderDot.addEventListener('click', showSecondQuote);
+thirdSliderDot.addEventListener('click', showThirdQuote);
+
+function showFirstQuote() {
+  if (!firstSliderDot.classList.contains('slider__dot--active')) {
+    firstSliderDot.classList.add('slider__dot--active');
+
+    secondSliderDot.classList.remove('slider__dot--active');
+    thirdSliderDot.classList.remove('slider__dot--active');
+
+    firstTestimonial.classList.add('testimonial-visible');
+    secondTestimonial.classList.remove('testimonial-visible');
+    thirdTestimonial.classList.remove('testimonial-visible');
+  }
+}
+
+function showSecondQuote() {
+  if (!secondSliderDot.classList.contains('slider__dot--active')) {
+    secondSliderDot.classList.add('slider__dot--active');
+
+    firstSliderDot.classList.remove('slider__dot--active');
+    thirdSliderDot.classList.remove('slider__dot--active');
+
+    secondTestimonial.classList.add('testimonial-visible');
+    firstTestimonial.classList.remove('testimonial-visible');
+    thirdTestimonial.classList.remove('testimonial-visible');
+  }
+}
+
+function showThirdQuote() {
+  if (!thirdSliderDot.classList.contains('slider__dot--active')) {
+    thirdSliderDot.classList.add('slider__dot--active');
+
+    firstSliderDot.classList.remove('slider__dot--active');
+    secondSliderDot.classList.remove('slider__dot--active');
+
+    thirdTestimonial.classList.add('testimonial-visible');
+    firstTestimonial.classList.remove('testimonial-visible');
+    secondTestimonial.classList.remove('testimonial-visible');
+  }
+}
